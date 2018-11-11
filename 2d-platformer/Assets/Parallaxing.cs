5dd2e99eacdb4e73592004d98fcfe0d54a98c03b
FeVi2014 <31832058+FeVi2014@users.noreply.github.com>
2018-11-11 21:26:37 -0200

    background parallax

---
+++ 2d-platformer/Assets/Parallaxing.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour {
+
+	private Transform mainCamera;
+	private Vector3 previousCameraPosition;
+	private float[] parallaxScales;
+
+	public Transform[] Backgrounds;
+	public float Smoothing = 1f;
+
+	void Awake () {
+		mainCamera = Camera.main.transform;
+	}
+
+	void Start () {
+		previousCameraPosition = mainCamera.position;
+        FillParallaxScales();
+	}
+	
+	void Update () {
+		ShiftBackgrounds();
+		previousCameraPosition = mainCamera.position;
+	}
+
+	void FillParallaxScales() {
+        parallaxScales = new float[Backgrounds.Length];
+
+        int cont = 0;
+        foreach (var background in Backgrounds)
+        {
+            parallaxScales[cont] = background.position.z * -1;
+            cont++;
+        }
+	}
+
+	void ShiftBackgrounds() {
+        float parallax;
+        float backgroundTargetPositionX;
+        Vector3 backgroundTargetPosition;
+
+        int cont = 0;
+        foreach (var background in Backgrounds)
+        {
+            parallax = (previousCameraPosition.x - mainCamera.position.x) * parallaxScales[cont];
+
+            backgroundTargetPositionX = background.position.x + parallax;
+
+            backgroundTargetPosition = new Vector3(backgroundTargetPositionX, background.position.y, background.position.z);
+
+            background.position = Vector3.Lerp(background.position, backgroundTargetPosition, Smoothing * Time.deltaTime);
+
+            cont++;
+        }
+	}
+}
